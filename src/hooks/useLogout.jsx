52eb13c41e1c4229a1c4b75802bf4991c775a143
Miruto80 <miguelfernando8012@gmail.com>
2026-09-09 21:07:03 -0400

    Corregido bug de carrito entre sesiones

--- src/hooks/useLogout.jsx
+++ src/hooks/useLogout.jsx
@@ -1,13 +1,16 @@
 import { useNavigate } from "react-router-dom";
 import { notifySuccess } from "../utils/Tostify";
+import { useCart } from "./useCart";
 
 export const useLogout = () => {
   const navigate = useNavigate();
+  const { clearCart } = useCart();
 
   return () => {
     localStorage.removeItem("accessToken");
     localStorage.removeItem("refreshToken");
     localStorage.removeItem("usuario");
+    clearCart();
     notifySuccess("Sesión cerrada correctamente");
     navigate("/login", { replace: true });
   };
